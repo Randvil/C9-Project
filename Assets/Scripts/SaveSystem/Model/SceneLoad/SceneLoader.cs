8efd5837f9700fef803f63ace76ffc2727585980
AnnaShln <anyashilnikova0708@gmail.com>
2023-04-19 14:56:15 +0300

    Создание игрока и камеры в комнате босса для тестирования локации

--- Assets/Scripts/SaveSystem/Model/SceneLoad/SceneLoader.cs
+++ Assets/Scripts/SaveSystem/Model/SceneLoad/SceneLoader.cs
@@ -11,6 +11,12 @@ public class SceneLoader : MonoBehaviour
 
     private void Awake()
     {
+        //only for testing
+        //remove later
+        NewGameSave newGame = GetComponent<NewGameSave>();
+        newGame.CreateNewGameSave();
+
+
         dataHandler = new FileDataHandler("Saves", "LastSave");
         gameData = dataHandler.Load();
 
